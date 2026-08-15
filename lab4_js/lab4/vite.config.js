9688a158db0ef0b8c21c360cf5912fbb735ce6ee
MILDlove3 <m.kanokwan2901@gmail.com>
2026-08-16 03:21:06 +0700

    Delete lab4_js/lab4 directory

--- lab4_js/lab4/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-export default defineConfig({
-    root: "www",
-    base: "/lab154377/lab4/",
-    build: {
-        outDir: "..154377/lab4",
-        emptyOutDir: true,
-    },
-});

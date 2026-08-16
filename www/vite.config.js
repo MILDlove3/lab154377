06512ab1f93247a153de1eca9da3e9c6480239ba
MILDlove3 <m.kanokwan2901@gmail.com>
2026-08-16 14:34:38 +0700

    Lab4-670410002

---
+++ www/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+    root: "www",
+    base: "/lab154377/lab4_js/lab4",
+    build: {
+        outDir: "../lab154377/lab4_js/lab4/dist",
+        emptyOutDir: true,
+    },
+});

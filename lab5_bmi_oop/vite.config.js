7ac009cb2df8ff3cf3ebaed450bc8f07c0c7d08e
MILDlove3 <m.kanokwan2901@gmail.com>
2026-08-17 01:19:11 +0700

    Lab5-670410002

---
+++ lab5_bmi_oop/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+export default defineConfig({
+    base: "/lab154377/lab5/",
+    build: {
+        outDir: "../lab154377/lab5/",
+        emptyOutDir: true,
+    },
+});
